38cc0637407c87353230fd05a99ce201fadaa972
wangpeng <w18454343991@gmail.com>
2017-07-22 12:54:17 +0800

    no message

--- c7/7_5.c
+++ c7/7_5.c
@@ -1,120 +1,162 @@
 #include <stdio.h>
 
-static int index = 1;//标志位数
 
 int main(void)
 {
-	void written_amount(unsigned int amount,char (*buffer)[5]);
-	unsigned int a;
-
-	char buffer[1024][5] = {
-		{'s','s','s','s','s'},
-		{'s','s','s','s','s'},
-	};
-
-	char (*p)[5] = buffer;
-    a = 221545;
+	void written_amount(long long int amount);
+	long long int a;
+    printf("你的数字:");
     
-    written_amount(a , p+1023);
-	
+    while(1){
+        scanf("%lld",&a);
+        if(a <= 999999999999)
+            break;
+        else{
+            printf("数据太大！\n");
+            printf("你的数字:");
+        }
+
+
+    }
+
+    if(a>0)
+        written_amount(a);
+    else if (a == 0)
+        printf("零");
+    else
+    {
+        printf("负 ");
+        written_amount(-a);
+    }	
 	return 0;
 }
 
 
-void written_amount(unsigned int amount,char (*buffer)[5])
+
+long long int cifang(int a,int b)
 {
+    long long int n = 1;
+    for (int i = 0; i < b; ++i)
+        n *= a;
+    return n;
+}
+
+
+
+void written_amount(long long amount)
+{
+    long long int cifang(int a,int b);
+    long long int index = 0;
+    int first = 0;
+    int in_zero = 1;
+
 
 	if (amount == 0){
-	    for (int i = 0; i < 1024; ++i)
-	        printf("%s",buffer[i]);
 	    printf("\n");
 	}
-	else{
-		int group = amount % 10000;
-        for (int i = 0; i < 4; ++i)
+	
+    else{
+		
+        int group;
+ 
+        for(int i = 1; i < 5; i++)
         {
-            int a = group % 10;
-            group /= 10;
-
-            if (a == 0 && i != 0)
-            	continue;
-            switch(i)
-            {
-            	case 1:
-            	   *buffer = "十";
-            	   buffer--;
-            	   break;
-            	case 2:
-            	   buffer = "百";
-            	   buffer--;
-            	   break;
-            	case 3:
-            	   buffer = "千";
-            	   buffer--;
-            	   break;
-            }
             
-            switch(a)
+            if (amount/cifang(10000,i) == 0)
             {
-            	case 0:
-            	buffer = "零";
-            	buffer--;
-            	break;
-            	case 1:
-            	buffer = "壹";
-            	buffer--;
-            	break;
-            	case 2:
-            	buffer = "贰";
-            	buffer--;
-            	break;
-            	case 3:
-            	buffer = "叁";
-            	buffer--;
-            	break;
-            	case 4:
-            	buffer = "肆";
-            	buffer--;
-            	break;
-            	case 5:
-            	buffer = "伍";
-            	buffer--;
-            	break;
-            	case 6:
-            	buffer = "陆";
-            	buffer--;
-            	break;
-            	case 7:
-            	buffer = "柒";
-            	buffer--;
-            	break;
-            	case 8:
-            	buffer = "仈";
-            	buffer--;
-            	break;
-            	case 9:
-            	buffer = "玖";
-            	buffer--;
-            	break;
-            }
-            if (group == 0)
-            {
-                index += 1;
-                switch(index)
-               {case 2:
-               	buffer = "万";
-               	buffer--;
-                break;
-                case 3:
-               	buffer = "亿";
-               	buffer--;
-                break;
-                }
+                group = amount / cifang(10000,i-1);
+                amount = amount % cifang(10000,i-1);
+                index = i;
                 break;
             }
         }
-           
-		return written_amount(amount/10000, buffer);
+        
+        for (int i = 3; i >= 0; i--)
+        {   
+            int a;
+
+            if((a = group /cifang(10,i)) == 0){
+                if(first == 1 && in_zero){
+                    printf("零");
+                    in_zero = 0;
+                }
+                continue;
+            }
+
+            first = 1;
+
+            group = group % cifang(10,i);
+
+
+            
+            switch(a)
+            {
+            	case 1:
+            	printf("壹");
+            	break;
+            	case 2:
+            	printf("贰");
+            	break;
+            	case 3:
+            	printf("叁");
+            	break;
+            	case 4:
+            	printf("肆");
+            	break;
+            	case 5:
+            	printf("伍");
+            	break;
+            	case 6:
+            	printf("陆");
+            	break;
+            	case 7:
+            	printf("柒");
+            	break;
+            	case 8:
+            	printf("仈");
+            	break;
+            	case 9:
+            	printf("玖");
+            	break;
+            }
+
+
+            switch(i)
+            {
+                case 1:
+                   printf("十");
+                   break;
+                case 2:
+                   printf("百");
+                   break;
+                case 3:
+                   printf("千");
+                   break;
+            }
+            
+
+            if (group == 0)
+            {
+            switch(index)
+               {
+                case 1:
+                index++;
+                break;
+                case 2:
+               	printf("万");
+                index += 1;
+                break;
+                case 3:
+                index += 1;
+               	printf("亿");
+                break;
+                }
+            break;
+            }
+
+
+        }
+		return written_amount(amount);
 	}
       
 }
